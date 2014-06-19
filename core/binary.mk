89866a8e125e132b66a27444c596386911dfa6e9
Marcus Tang <tmc9031@gmail.com>
2014-06-20 00:09:39 +0800

    up: support c include dir

--- core/binary.mk
+++ core/binary.mk
@@ -1,6 +1,10 @@
 c_binary := $(LOCAL_PATH)/$(LOCAL_MODULE)
 c_objects := $(patsubst %.c,$(LOCAL_PATH)/%.o,$(LOCAL_SRC_FILES))
 c_deps := $(patsubst %.c,$(LOCAL_PATH)/%.d,$(LOCAL_SRC_FILES))
+ifneq ($(LOCAL_C_INCLUDES),)
+c_includes := $(addprefix -I ,$(LOCAL_C_INCLUDES))
+#c_includes := $(addprefix -I ,$(addprefix $(LOCAL_PATH)/,$(LOCAL_C_INCLUDES)))
+endif # LOCAL_C_INCLUDES
 
 $(DEFAULT_GOAL): $(c_binary)
 #$(DEFAULT_GOAL):
@@ -16,12 +20,14 @@ $(c_binary): $(c_objects)
 #	$(CC) $(CFLAGS) -o $@ $^
 	gcc -o $@ $^
 	@echo '>>> Finished building target: $@'
+	@echo ' '
 
 $(c_objects): %.o: %.c
 	@echo '>>> Building file: $<'
 #	$(CC) $(CFLAGS) -o $@ -c $<
-	gcc -o $@ -c $< -MMD -MF $(patsubst %.o,%.d,$@)
+	gcc -o $@ -c $< -MMD -MF $(patsubst %.o,%.d,$@) $(c_includes)
 #	gcc -o $@ -c $< -MMD -MP -MF $(patsubst %.o,%.d,$@) -MT $(patsubst %.o,%.d,$@)
+	@echo ' '
 
 -include $(c_deps)
 
